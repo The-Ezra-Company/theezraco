928282379f45baf01d484bbcc82cf3b92094a446
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-05-07 16:41:11 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black text-shadow-[0_15px_25px_rgb(244_237_233_/_0.25)]">
+      <h1 className="text-9xl font-bold text-[rgb(244,237,233)]">Coming Soon...</h1>
+    </div>
+  )
+}
