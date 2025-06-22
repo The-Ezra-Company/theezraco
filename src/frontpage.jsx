d99813c03b77f4a89cf6abca9e2d1b23eabc9443
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-06-21 22:35:14 -0600

    Completed landing page

--- src/frontpage.jsx
+++ src/frontpage.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { FaSquareFacebook, FaInstagram, FaXTwitter } from "react-icons/fa6";
 
 export default function Frontpage() {
   // Playback useEffect
@@ -14,7 +15,7 @@ export default function Frontpage() {
     <div className="relative h-screen w-full overflow-hidden bg-black">
       {/* Background video */}
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        className="absolute top-0 left-0 w-full h-full object-cover z-0 blur-sm"
         src="/bg-video_1.mp4"
         autoPlay
         loop
@@ -24,11 +25,37 @@ export default function Frontpage() {
         id="bg-video"
       />
 
-      {/* Overlay content */}
-      <div className="relative z-10 flex justify-center items-center h-full text-shadow-[0_15px_25px_rgb(244_237_233_/_0.25)]">
-        <h1 className="text-9xl font-bold text-[rgb(244,237,233)]">
-          Coming Soon...
-        </h1>
+      <div className="flex h-screen flex-col items-center justify-center space-y-15 z-10 relative">
+        <p className="roboto-bold-italic  text-[256px] leading-none">
+          EZRA
+        </p>
+        <p className="merriweather-normal  text-center text-[64px] leading-none">
+          Launching
+          <br />
+          Soon!
+        </p>
+
+        <div className="flex flex-col items-center space-y-2 ">
+          <p className="inter-normal  text-[32px]">
+            Connecting Believers Under One Platform
+          </p>
+          <div className="inter-normal rounded-lg  px-4 py-1 text-[24px]">
+            Contact Us
+          </div>
+          <p className="inter-bold ">info@theezraco.com</p>
+
+          <div className="flex space-x-4 ">
+            <div className=" text-[32px]">
+              <FaSquareFacebook />
+            </div>
+            <div className=" text-[32px]">
+              <FaInstagram />
+            </div>
+            <div className=" text-[32px]">
+              <FaXTwitter />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
