a38e2c3545e5c56ef35a86b3dbfa845c8675a874
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-06-21 22:42:20 -0600

    Added LinkedIn

--- src/frontpage.jsx
+++ src/frontpage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { FaSquareFacebook, FaInstagram, FaXTwitter } from "react-icons/fa6";
+import { FaSquareFacebook, FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
 
 export default function Frontpage() {
   // Playback useEffect
@@ -12,10 +12,10 @@ export default function Frontpage() {
 
   // **** RENDER ****
   return (
-    <div className="relative h-screen w-full overflow-hidden bg-black">
+    <div className="relative h-screen w-full overflow-hidden bg-black text-center">
       {/* Background video */}
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover z-0 blur-sm"
+        className="absolute top-0 left-0 z-0 h-full w-full object-cover blur-sm"
         src="/bg-video_1.mp4"
         autoPlay
         loop
@@ -25,34 +25,38 @@ export default function Frontpage() {
         id="bg-video"
       />
 
-      <div className="flex h-screen flex-col items-center justify-center space-y-15 z-10 relative">
-        <p className="roboto-bold-italic  text-[256px] leading-none">
-          EZRA
-        </p>
-        <p className="merriweather-normal  text-center text-[64px] leading-none">
+      {/* Front Content */}
+      <div className="relative z-10 flex h-screen flex-col items-center justify-center space-y-15">
+        <p className="roboto-bold-italic text-[256px] leading-none">EZRA</p>
+        <p className="merriweather-normal text-[64px] leading-none">
           Launching
           <br />
           Soon!
         </p>
 
-        <div className="flex flex-col items-center space-y-2 ">
-          <p className="inter-normal  text-[32px]">
+        {/* Contact Info */}
+        <div className="flex flex-col items-center space-y-2">
+          <p className="inter-normal text-[32px]">
             Connecting Believers Under One Platform
           </p>
-          <div className="inter-normal rounded-lg  px-4 py-1 text-[24px]">
+          <div className="inter-normal rounded-lg px-4 py-1 text-[24px]">
             Contact Us
           </div>
-          <p className="inter-bold ">info@theezraco.com</p>
+          <p className="inter-bold">info@theezraco.com</p>
 
-          <div className="flex space-x-4 ">
-            <div className=" text-[32px]">
+          {/* Socials */}
+          <div className="flex space-x-4">
+            <div className="text-[32px]">
               <FaSquareFacebook />
             </div>
-            <div className=" text-[32px]">
+            <div className="text-[32px]">
               <FaInstagram />
             </div>
-            <div className=" text-[32px]">
+            <div className="text-[32px]">
               <FaXTwitter />
+            </div>
+            <div className="text-[32px]">
+              <FaLinkedin />
             </div>
           </div>
         </div>
