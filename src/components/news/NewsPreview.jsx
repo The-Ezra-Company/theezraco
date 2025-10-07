69deb2a29d83b95fc2883d78ac99fdc8e02814d8
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-10-07 10:52:35 -0700

    Fixed default image previews

--- src/components/news/NewsPreview.jsx
+++ src/components/news/NewsPreview.jsx
@@ -1,10 +1,15 @@
+import { COMPANY_NAME } from "../../config.js";
 import { useEffect, useRef, useState } from "react";
 
 export default function NewsPreview({ title, text }) {
   return (
     <div className="aspect-square overflow-hidden rounded-2xl bg-gradient-to-br from-gray-600 to-gray-500 shadow-lg backdrop-blur-md">
-      {/* Top Half - Image Placeholder */}
-      <div className="h-1/2 bg-blue-300 lg:h-2/3" />
+      {/* Top Half - Default image */}
+      <div className="my-auto flex h-1/2 w-full items-center justify-center overflow-hidden bg-gradient-to-br from-[#9c6e5b] to-[#ffc194] lg:h-2/3">
+        <div className="roboto-bold-italic text-center text-[96px] tracking-[-0.1em] md:text-[96px] lg:text-[136px]">
+          {COMPANY_NAME}
+        </div>
+      </div>
 
       {/* Bottom Half - Text */}
       <div className="flex h-1/2 flex-col p-4 lg:h-1/3">
@@ -24,7 +29,7 @@ export default function NewsPreview({ title, text }) {
         </div>
 
         {/* Body text with bottom fade */}
-        <p className="leading-[1.5] relative mt-2 flex-1 overflow-hidden text-sm text-ellipsis text-gray-200">
+        <p className="relative mt-2 flex-1 overflow-hidden text-sm leading-[1.5] text-ellipsis text-gray-200">
           {text}
         </p>
       </div>
