229af8466387365091d69ac1ffbb6ea71f460ff3
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-10-24 17:37:02 -0700

    Updates

--- assets/index-SRKL20jj.js
+++ assets/index-SRKL20jj.js
@@ -77,13 +77,13 @@ Ezra’s mission is simple yet profound: **bring users under one platform**. By 
 
 ## Our Designs!
 
-![](start_page.png)
+![](https://raw.githubusercontent.com/The-Ezra-Company/newsletter/main/bridging_spirituality/start_page.png)
 *Ezra's startup page*
 
-![](home_page.png)
+![](https://raw.githubusercontent.com/The-Ezra-Company/newsletter/main/bridging_spirituality/home_page.png)
 *A home page with personalized content for your faith*
 
-![](community.png)
+![](https://raw.githubusercontent.com/The-Ezra-Company/newsletter/main/bridging_spirituality/community.png)
 *View a community's page on Ezra!*`,xv=`---
 title: "We are the EZRA Company"
 description: "Redefining Community for the Next Generation of Believers"
